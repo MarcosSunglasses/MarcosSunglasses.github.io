a0c4e216287a8e2411bfdfe62f218babaab2cb02
MarcosSunglasses <marcossunglasses@gmail.com>
2025-05-05 11:22:37 -0400

    MarcosSunglasses
    Web

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const img = document.querySelector('.profile-img');
 
-    // Lista de imágenes
+
     const images = [
         'Archivos/Marcos.png',
         'Archivos/Marcos.png',
         'Archivos/Marcos.png'
     ];
 
-    // Cambiar imagen aleatoriamente cada 2 segundos
+
     setInterval(() => {
         const randomImage = images[Math.floor(Math.random() * images.length)];
         img.src = randomImage;
